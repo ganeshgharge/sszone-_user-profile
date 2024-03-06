6a0bbf574fd01ba508f9e907abbc61436babef9d
ganeshgharge <154337490+ganeshgharge@users.noreply.github.com>
2024-03-06 20:38:16 +0530

    Add files via upload

---
+++ Doctor_Appointment/app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from app import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns =[
+    path('',views.header,name="user-profile")
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
